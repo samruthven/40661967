78a6f1545cb9fe8a5afcf1ba6243177984dea639
samruthven <sam.ruthven789@gmail.com>
2025-07-18 22:06:18 +0100

    Update scene05.js

--- Scene 05/scene05.js
+++ Scene 05/scene05.js
@@ -1,3 +1,3 @@
-document.getElementById('scene05-button').onclick = function () {
+document.getElementById('scene05b-button').onclick = function () {
 	window.location.href = 'https://samruthven.github.io/40661967/Scene 06/scene06.html'
 };
