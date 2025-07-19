650d47c7defc04cd403484d6751bbd1e0950fa54
samruthven <sam.ruthven789@gmail.com>
2025-07-19 09:33:54 +0100

    Update and rename scene04.js to scene04.js

--- Scene 04/scene04.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('scene04c-button').onclick = function () {
-	window.location.href = 'https://samruthven.github.io/40661967/Scene 05/scene05.html'
-};
---
+++ Scene04/scene04.js
@@ -0,0 +1,3 @@
+document.getElementById('scene04c-button').onclick = function () {
+	window.location.href = 'https://samruthven.github.io/40661967/Scene05/scene05.html'
+};
