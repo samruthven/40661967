ce856a63a94d38356eb0ccc80d5407a0a552a522
samruthven <sam.ruthven789@gmail.com>
2025-07-19 09:46:42 +0100

    Update and rename scene14.js to scene14.js

--- Scene 14/scene14.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('scene14-button').onclick = function () {
-	window.location.href = 'https://samruthven.github.io/40661967/Scene 15/scene15.html'
-};
---
+++ Scene14/scene14.js
@@ -0,0 +1,3 @@
+document.getElementById('scene14-button').onclick = function () {
+	window.location.href = 'https://samruthven.github.io/40661967/Scene15/scene15.html'
+};
