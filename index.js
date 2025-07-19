6ea68d4b60e96c1b557a6a0da1d604d47cc63ed5
samruthven <sam.ruthven789@gmail.com>
2025-07-19 09:49:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById('start-button').onclick = function () {
-	window.location.href = 'https://samruthven.github.io/40661967/Scene 01/scene01.html'
+	window.location.href = 'https://samruthven.github.io/40661967/Scene01/scene01.html'
 };
 
